6f4222c09bf2b19af6291fd8b0a9e7a20a56d7b5
darwin <darwindelacruz0012@gmail.com>
2022-12-03 18:29:56 +0800

    Initial Commit

---
+++ function.py
@@ -0,0 +1,23 @@
+FILEPATH = r"C:\Users\darwi\PycharmProjects\mytodos\todos\todos.txt"
+
+
+def get_todos():
+    """
+    Open todos.txt textfile and return list of to-do items
+    """
+    with open(FILEPATH, 'r') as file:
+        todos = file.readlines()
+    return todos
+
+
+def write_todos(todos_local):
+    """
+    Write/update todos.txt textfile
+    """
+    with open(FILEPATH, 'w') as file:
+        file.writelines(todos_local)
+
+
+if __name__ == "__main__":
+    print("Hello")
+
